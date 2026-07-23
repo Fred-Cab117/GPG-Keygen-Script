1d78354fdc7db264977589ffc752324d87d55572
Fred-Cab117 <cabellfrederick@gmail.com>
2026-07-23 18:59:02 -0500

    Add files via upload

---
+++ keygen-script.sh
@@ -0,0 +1,15 @@
+echo -n  "Enter a name: "
+read name
+echo -n "Enter an email: "
+read email
+
+gpg --batch --full-generate-key --passphrase '' <<EOF
+Key-Type: RSA
+Key-Length: 2048
+Name-Real: ${name}
+Name-Email: ${email}
+Expire-Date: 0
+%commit
+EOF
+echo "Key pair generated"
+gpg --export --armor --output ${email} >> "${name}-public.txt"
